6df129f7dacab8981819d988dbe0db7bdff59f78
shbandan <47702808+shbandan@users.noreply.github.com>
2019-02-22 20:37:21 -0500

    Update database.config.js

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: 'mongodb://admin:mongopwd@10.128.24.96:27017/credit-card-detail?authSource=admin'
+    url: 'mongodb://10.128.24.96:27017/credit-card-detail?authSource=admin'
 };
