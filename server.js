40e2402b6970e3c569c0b3a6615c2cadd9b31200
Vishal  Ranapariya <vishalranapariya@Vishals-MBP.fios-router.home>
2019-02-28 18:23:33 -0500

    added new

--- server.js
+++ server.js
@@ -22,9 +22,41 @@ mongoose.connect(dbConfig.url, {
     process.exit();
 });
 
+console.log(mongoUrl);
+
+var mongoHost, mongoPort, mongoDatabase, mongoPassword, mongoUser;
+if (process.env.DATABASE_SERVICE_NAME) {
+    var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase();
+    mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'];
+    mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'];
+    mongoDatabase = process.env[mongoServiceName + '_DATABASE'];
+    mongoPassword = process.env[mongoServiceName + '_PASSWORD'];
+    mongoUser = process.env[mongoServiceName + '_USER'];
+
+  // If using env vars from secret from service binding  
+  } else if (process.env.database_name) {
+    mongoDatabase = process.env.database_name;
+    mongoPassword = process.env.password;
+    mongoUser = process.env.username;
+    var mongoUriParts = process.env.uri && process.env.uri.split("//");
+    if (mongoUriParts.length == 2) {
+      mongoUriParts = mongoUriParts[1].split(":");
+      if (mongoUriParts && mongoUriParts.length == 2) {
+        mongoHost = mongoUriParts[0];
+        mongoPort = mongoUriParts[1];
+      }
+    }
+  }
+
+  console.log(mongoHost);
+  console.log(mongoPort);
+  console.log(mongoDatabase);
+  console.log(mongoPassword);
+  console.log(mongoUser);
+  console.log(mongoUriParts);
 
 app.get('/', (req,res) => {
-    res.json({"message": mongoUrl});
+    res.json({"message": "Welcome to retail application demo"});
 });
 
 require('./app/routes/user.routes.js') (app);
